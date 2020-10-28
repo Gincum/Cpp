99881f3886b1d4504423554cd0d6e95024db2ff9
Gincum <buemio@inwind.it>
2020-10-28 16:50:37 +0100

    Add files via upload

---
+++ Arrays.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namesplace std;
+
+int main(){
+	
+	string in;
+	int Ni[2], Nt[2]; //Ni sono le grandezze dei due array AR1 e AR2 mentre Nt quanti valori dei due array AR1 e AR2 devo prendere per fare il confronto
+	int Ar1[100001], Ar2[100001];
+	int appoggio;
+	
+	cout << "Ni1 Ni2";
+	cin >> in; // qui sto inserendo Ni
+	
+	// devi trasformare in string togliendo lo spazio e memorizzare in array
+	
+	cout << "Nt1 Nt2";
+	cin >> in // qui sto inserendo Nt
+	
+	// devi trasformare in string togliendo lo spazio e memorizzare in array
+	
+}
